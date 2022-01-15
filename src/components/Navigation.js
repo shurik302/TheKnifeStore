f497a35de5a35822c4af6ee9b9a9003555bc557a
shurik302 <balabuha1988@gmail.com>
2022-01-15 14:30:32 +0200

    4 апдейт , добавил сёрчбар и катаолг

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import '../stylesheets/Components.css';
-
+import imgLogo from '../images/icon.png'
 
 function Navigation() {
     return (
-        <div>
+        <div className='navigHead'>
             <nav>
           <ul className='navUl'>
             <li className='navUlHome'>
@@ -27,6 +27,41 @@ function Navigation() {
                 <i class="fas fa-bars"></i>
             </li>
           </ul>
+          <div className='loubarHead'>
+            <div className='imgHead'>
+              <Link to="/"><img src={imgLogo}/></Link>
+            </div>
+            <div className='searchLine'>
+              <form>
+                <input type='search' placeholder='поиск'/>
+                <i class="fas fa-search"></i>
+              </form>
+            </div>
+            <div className='locationHead'>
+              <Link to=""><i class="fas fa-map-marker-alt"></i><span>москва</span></Link>
+            </div>
+            <div className='telHead'>
+              <span>8-800-777-49-67<i class="fas fa-chevron-down"></i></span> 
+              <Link to=""><span>Заказать звонок</span></Link>
+            </div>
+            <div className='heartAndCartHead'>
+              <i class="far fa-heart"></i>
+              <i class="fas fa-shopping-cart"></i>
+              <div className='moneyHead'>
+                <span>0 р.</span>
+                <Link to=""><span>Оформить заказ</span></Link>
+              </div>
+            </div>
+          </div>
+          <div className='catalogeHead'>
+            <ol>
+              <li>Каталог ножей</li>
+              <li>Клинковое оружие</li>
+              <li>Сувенирные изделия</li>
+              <li>Фонари ARMYTEK</li>
+              <li>Сопутствующие товары</li>
+            </ol>
+          </div>
           
         </nav>
 
