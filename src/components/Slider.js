c3625d6585cb0fcb3a84655816cd5041499ae0fb
shurik302 <balabuha1988@gmail.com>
2022-02-05 14:37:12 +0200

    доделал слайдер

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
+import '../stylesheets/Components.css'
 
 export default class SimpleSlider extends Component {
   render() {
@@ -8,26 +9,42 @@ export default class SimpleSlider extends Component {
       infinite: true,
       speed: 500,
       slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      autoplay:true,
+      autoplaySpeed: 15000
     };
     return (
-      <div>
+      <div className="AllSlide">
         <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
         <style>{cssstyle}</style>
         <Slider {...settings}>
-          <div>
-            <h3>TEST 1 slide</h3>
-          </div>
-          <div>
-            <h3>TEST 2 slide</h3>
-          </div>
-          <div>
-            <h3>TEST 3 slide</h3>
-          </div>
-          <div>
-            <h3>TEST 4 slide</h3>
-          </div>
+          
+            <div>
+              <span className="title">Интернет магазин сертифицированных</span><br/>  
+              <span className="titleBott">златоустовских ножей</span><br/>
+              <span className="allDescription"><span className="descriptionSlider">Добро пожаловать на официальный сайт «ЗЛАТМАКС»! В нашем магазине </span><br/>представлен наиболее широкий выбор Златоустовских ножей от Златоустовских <br/>Оружейных Фабрик и компаний, мы являемся официальными поставщиками.</span>
+              <a href="#" className="TitleButton">Подробнее</a>
+            </div>
+            <div>
+              <span className="title">Интернет магазин сертифицированных</span><br/>  
+              <span className="titleBott">златоустовских ножей</span><br/>
+              <span className="allDescription"><span className="descriptionSlider">Добро пожаловать на официальный сайт «ЗЛАТМАКС»! В нашем магазине </span><br/>представлен наиболее широкий выбор Златоустовских ножей от Златоустовских <br/>Оружейных Фабрик и компаний, мы являемся официальными поставщиками.</span>
+              <a href="#" className="TitleButton">Подробнее</a>
+            </div>
+            <div>
+              <span className="title">Интернет магазин сертифицированных</span><br/>  
+              <span className="titleBott">златоустовских ножей</span><br/>
+              <span className="allDescription"><span className="descriptionSlider">Добро пожаловать на официальный сайт «ЗЛАТМАКС»! В нашем магазине </span><br/>представлен наиболее широкий выбор Златоустовских ножей от Златоустовских <br/>Оружейных Фабрик и компаний, мы являемся официальными поставщиками.</span>
+              <a href="#" className="TitleButton">Подробнее</a>
+            </div>
+            <div>
+              <span className="title">Интернет магазин сертифицированных</span><br/>  
+              <span className="titleBott">златоустовских ножей</span><br/>
+              <span className="allDescription"><span className="descriptionSlider">Добро пожаловать на официальный сайт «ЗЛАТМАКС»! В нашем магазине </span><br/>представлен наиболее широкий выбор Златоустовских ножей от Златоустовских <br/>Оружейных Фабрик и компаний, мы являемся официальными поставщиками.</span>
+              <a href="#" className="TitleButton">Подробнее</a>
+            </div>
+          
         </Slider>
       </div>
     );
@@ -37,19 +54,27 @@ export default class SimpleSlider extends Component {
 const cssstyle = `
 
 .slick-dots li{
-    margin:80px;
+    margin:0 15px;
 }
 
+.slick-dots{
+  max-width:212px;
+  margin-left: 48px;
 
-
-
+}
+.slick-next:before, .slick-prev:before {
+  content: "";
+  font-size: 0;
+}
 .slick-dots li button {
-    width: 60px;
-    height: 20px;
+  font-size: 0;
+  line-height: 0;
+  width: 20px;
+  height: 2px;
+  padding: 0;
     /* border-radius: 50%; */
     background-color: rgba(124, 121, 121, 0.336);
     text-indent: -9999px; //убрать точки
-    margin:80px;
 }
 
 li.slick-active button {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import SimpleSlider from '../components/Slider';
+import '../stylesheets/Home.css';
+import artage from '../images/artage-io-thumb-.png';
+import verify from '../images/verify.png'
 
 function Home() {
     return (
-        <div>
-            <SimpleSlider/>
+        <div className='allTitle'>
+            <div className='slider'>
+                <SimpleSlider />
+                <div className='knifeTitle'>
+                    <img src={artage}/>
+                </div>
+                <div className='circle'></div>
+            </div>
+            <div className='bottomTitle'>
+                <div>
+                    <img src={verify}/>
+                    <span>Гарантия 100% возврата <br/>денежных средств</span>
+                </div>
+                <div>
+                    <img src={artage}/>
+                    <span></span>
+                </div>
+                <div>
+                    <img src={artage}/>
+                    <span></span>
+                </div>
+                <div>
+                    <img src={artage}/>
+                    <span></span>
+                </div>
+            </div>
+            
         </div>
     )
 }
